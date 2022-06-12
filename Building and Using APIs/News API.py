a33f0720e3fba3e8e605b21ec32999855c681482
buggy <tekhnetos42@gmail.com>
2022-06-12 12:16:14 -0400

    final

--- Building and Using APIs/News API.py
+++ Building and Using APIs/News API.py
@@ -6,12 +6,26 @@ import os
 sys.path.insert(1, '../..')
 import creds
 
+def get_news(topic, from_date, to_date, language='en', api_key=creds.api_key):
+    # the api page, where everything is html showing json
+    url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json()
 
-# the api page, where everything is html showing json
-r = requests.get(f'https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-6-10&to=2022-6-12&sortBy=popularity&language=en&apiKey={creds.api_key}')
-content = r.json()
+    # content is converted to json. we look for the article dictionary, then
+    # the first article, and then just the title. json is how apis mostly talk
+    articles = content['articles']
+    results = []
+    for article in articles:
+        results.append(f"TITLE\n', {article['title']}, '\nDESCRIPTION\n', {article['description']}")
 
-# content is converted to json. we look for the article dictionary, then
-# the first article, and then just the title
-print(content['articles'][0]['title'])
-os.system("pyclean . -q")
+        # # prints title and description
+        # print('TITLE\n', article['title'], '\nDESCRIPTION\n', article['description'])
+
+    return results
+
+# print and call the function. set parameters
+print(get_news(topic='space', from_date='2022-5-27', to_date='2022-5-28'))
+
+# cleanup
+os.system("pyclean ../.. -q")
